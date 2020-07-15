4ecbe61a3e12473b113cd65ad78c7b8177317754
Austin Hill <austinkinghill@gmail.com>
2020-07-14 17:04:31 -0700

    build v0.1

---
+++ build/precache-manifest.ffea7e77c67adaf2d9e729a81e97c17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b6642827fe2450d4bd09c9f0949553",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d3794b6191e0be76ad0",
+    "url": "/static/css/main.213beffc.chunk.css"
+  },
+  {
+    "revision": "3a3b7c1c5dce2b67aafd",
+    "url": "/static/js/2.93cde26d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.93cde26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3794b6191e0be76ad0",
+    "url": "/static/js/main.08072a85.chunk.js"
+  },
+  {
+    "revision": "f1adb366704c43014c6d",
+    "url": "/static/js/runtime-main.9fe9cd45.js"
+  }
+]);
