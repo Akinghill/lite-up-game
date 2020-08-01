2e1fdb23f323934a0961efc265641f8a585af1a7
Austin Hill <austinkinghill@gmail.com>
2020-08-01 10:22:25 -0700

    send high scores to api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ScoreBoard from './components/ScoreBoard/ScoreBoard';
 import Login from './components/Login/Login';
 import Register from './components/Login/Register';
 import Game from './components/Game/Game';
-// import Timer from './components/Timer/Timer';
 import Debugger from './components/Debugger/Debugger';
 import {
   randomizeGameBoard,
@@ -13,22 +12,6 @@ import {
   isGameWon,
   processInput,
 } from './logic/gameLogic';
-
-const initialState = {
-  gameWon: false,
-  score: 0,
-  gameState: [],
-  route: 'home',
-  highScoreData: [],
-  isSignedIn: false,
-  user: {
-    id: '',
-    name: '',
-    email: '',
-    entries: 0,
-    joined: '',
-  },
-};
 
 class App extends Component {
   constructor(props) {
@@ -44,7 +27,7 @@ class App extends Component {
         id: '',
         name: '',
         email: '',
-        entries: 0,
+        scores: [],
         joined: '',
       },
     };
@@ -56,6 +39,7 @@ class App extends Component {
         id: data.id,
         name: data.name,
         email: data.email,
+        scores: data.scores,
       },
     });
   };
@@ -63,9 +47,15 @@ class App extends Component {
   handleClick = (e) => {
     let id = e.target.id;
     if (id === 'debugger') {
-      this.setState({
-        gameWon: true,
-      });
+      const updateUserScore = this.state.user
+      updateUserScore.scores.push(this.state.score)
+      this.setState(
+        {
+          user: updateUserScore,
+          gameWon: true,
+        },
+        () => this.postScore()
+      );
     }
     if (id === 'restart' || id === 'restartIcon' || id === 'play-again') {
       this.setState({
@@ -82,14 +72,20 @@ class App extends Component {
     // Verify a game square was selected.
     if (gameSquareSelected(id)) {
       let newGameState = this.state.gameState;
-      let processedGameState = processInput(id, newGameState)
+      let processedGameState = processInput(id, newGameState);
 
       if (isGameWon(this.state)) {
-        this.setState({
-          gameWon: true,
-        });
+        const updateUserScore = this.state.user
+        updateUserScore.scores.push(this.state.score)
+        this.setState(
+          {
+            user: updateUserScore,
+            gameWon: true,
+          },
+          () => this.postScore()
+        );
       }
-    
+
       this.setState({
         score: Number(this.state.score) + 1,
         gameState: processedGameState,
@@ -109,6 +105,25 @@ class App extends Component {
     });
   };
 
+  postScore() {
+    if (this.state.gameWon && this.state.user.email !== '') {
+      const data = {
+        user: this.state.user,
+      };
+
+      fetch('http://localhost:5000/users/newscore', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((user) => {
+          this.loadUser(user)
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
   render() {
     const { score, gameState, gameWon, route, user } = this.state;
     return (
@@ -116,7 +131,7 @@ class App extends Component {
         {route === 'register' ? (
           <Register onRouteChange={this.onRouteChange} />
         ) : route === 'login' ? (
-          <Login onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
+          <Login onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
         ) : null}
         <Game
           score={score}
@@ -133,3 +148,19 @@ class App extends Component {
 }
 
 export default App;
+
+// const initialState = {
+//   gameWon: false,
+//   score: 0,
+//   gameState: [],
+//   route: 'home',
+//   highScoreData: [],
+//   isSignedIn: false,
+//   user: {
+//     id: '',
+//     name: '',
+//     email: '',
+//     entries: 0,
+//     joined: '',
+//   },
+// };
